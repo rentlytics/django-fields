694b95d4c61a1557d4acd59775733b2a6c488b84
AmySchneider510 <amy@rentlytics.com>
2018-11-01 14:28:30 -0700

    Change pycrypto to pycryptodomex

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-version = '0.3.0'
+version = '0.3.1'
 
 setup(
     name='django-fields',
@@ -13,7 +13,7 @@ setup(
     url='http://github.com/svetlyak40wt/django-fields/',
     install_requires=[
         'django',
-        'pycrypto',
+        'pycryptodomex',
         'nose',
         'django-nose==1.4.4',
         'tox',
